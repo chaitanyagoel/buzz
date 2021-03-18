186bba38cc5ffdfc7c28fb612f4242c22c794385
Chaitanya goel <75871756+chaitanyagoel@users.noreply.github.com>
2021-03-18 15:24:10 +0530

    Add files via upload

---
+++ in.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Instagram extends React.Component{
+render(){
+    return(
+        <Text>Instagram page</Text>
+    )
+}
+
+
+
+}
